e1b2d80b3e1b901f012dc1f2641fb4870d3296d5
ohager <oliver@devbutze.com>
2026-07-27 19:30:32 -0300

    feat(character): render CharacterCreateForm on /character/create

--- pages/character/create.tsx
+++ pages/character/create.tsx
@@ -1,10 +1,10 @@
 import Page from '@components/Page';
-import CharacterCreateWizard from '@components/Character/CreateWizard/CharacterCreateWizard';
+import CharacterCreateForm from '@components/Character/CharacterCreateForm';
 
 const CharacterCreatePage = () => (
     <Page title="Create Character - SIGNArank" description="Create your on-chain Character on SignaRank.">
         <div className="content max-w-lg mx-auto py-8 px-4">
-            <CharacterCreateWizard />
+            <CharacterCreateForm />
         </div>
     </Page>
 );
